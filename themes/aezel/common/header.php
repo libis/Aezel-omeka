8c76dee7917060992ca6548a22819ce7a89d6d09
Joris <joris.lambrechts@bib.kuleuven.be>
2016-12-08 09:24:23 +0100

    update layout

--- themes/aezel/common/header.php
+++ themes/aezel/common/header.php
@@ -23,9 +23,8 @@
 
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,200' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Lora:400,400italic,700' rel='stylesheet' type='text/css'>    
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,400italic,700' rel='stylesheet' type='text/css'>
 
-    
     <!-- Stylesheets -->
     <?php
     queue_css_file(array('iconfonts','style'));
@@ -34,45 +33,33 @@
 
     echo theme_header_background();
     ?>
-      
+
     <!-- JavaScripts -->
-    <?php 
-    queue_js_file('bootstrap.min');
-    echo head_js(); 
+    <?php
+    echo head_js();
     ?>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous"></script>
+
 </head>
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
     <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
     <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
-    
-    <nav class="navbar navbar-default navbar-static-top">
-      <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-       
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
 
-             <?php echo link_to_home_page(theme_logo().option('site_title'),array('class'=>'navbar-brand')); ?>
-            </div>
-           
-        
-        <!-- Collect the nav links, forms, and other content for toggling -->
-          
-            <div id="navbar" class="navbar-collapse collapse no-padding-left">
-               <?php //echo public_nav_main(array('role' => 'navigation'))->setUlClass('nav navbar-nav');; ?>  
-                <?php echo public_nav_main_bootstrap(); ?>
-                <form class="navbar-form navbar-right" role="search">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search">
-                    </div>
-                    <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></i>
-                    </button>
-                </form>   
-            </div><!-- /.navbar-collapse -->   
+    <nav class="navbar navbar-default ">
+        <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+
+            <?php echo link_to_home_page(theme_logo().option('site_title'),array('class'=>'navbar-brand')); ?>
+
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <?php echo public_nav_main_bootstrap(); ?>
+            <form class="form-inline pull-xs-right" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default"><i class="fa fa-search" aria-hidden="true"></i>
+                </button>
+            </form>
       </div><!-- /.container-fluid -->
     </nav>
--- themes/aezel/index.php
+++ themes/aezel/index.php
@@ -2,8 +2,8 @@
 <div class="jumbotron">
     <div class="container">
         <div class="row">
-            <section class="kijker">                      
-                <div class="col-sm-6">
+            <section class="kijker">
+                <div class="col-sm-8">
                     <div class="carousel-wrap">
                         <div data-ride="carousel" class="carousel slide" id="myCarousel">
                           <!-- Indicators -->
@@ -12,21 +12,19 @@
                             <li data-slide-to="1" data-target="#myCarousel" class="active"></li>
                           </ol>
                           <div role="listbox" class="carousel-inner">
-                            <div class="item">
-                              <img alt="First slide" src="<?php echo img('bg6.png');?>" class="first-slide">
-
+                              <div class="carousel-item">
+                                <img alt="First slide" src="<?php echo img('bg6.png');?>" class="first-slide">
                                 <div class="carousel-caption">
-                                  <H3><span>In de kijker</span></h3>   
+                                  <H3><span>In de kijker</span></h3>
                                   <h1>Titel volgend verhaal</h1>
                                   <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
                                   <p class="caption-auteur">Door: <strong>Tester</strong></p>
-                                </div>   
+                                </div>
                             </div>
-
-                            <div class="item active">
-                              <img alt="Third slide" src="<?php echo img('bg5.jpg');?>" class="third-slide">                      
-                                <div class="carousel-caption">  
-                                   <H3><span>In de kijker</span></h3>   
+                            <div class="carousel-item active">
+                              <img alt="Third slide" src="<?php echo img('bg5.jpg');?>" class="third-slide">
+                                <div class="carousel-caption">
+                                   <H3><span>In de kijker</span></h3>
                                   <h1>Titel verhaal</h1>
                                   <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
                                   <p class="caption-auteur">Door: <strong>Tester2</strong></p>
@@ -36,127 +34,116 @@
                         </div>
                     </div>
                 </div>
-                <div id="right" class="col-sm-6">
+                <div id="right" class="col-sm-4">
                     <div class="well"></div>
                 </div>
-           </section>    
-       </div>   
-    </div>    
+           </section>
+       </div>
+    </div>
 </div>
 
-   <section class='section-icons'>   <!-- Content -->
+<section class='section-icons'>   <!-- Content -->
     <div class="container content-wrapper bs-docs-section ">
         <?php fire_plugin_hook('public_content_top', array('view'=>$this)); ?>
-
-
-        <h3  class="ontdek"><span>Ontdek onze projecten</span></h3>
-        <div class="row icons">    
-                  
-                <div class="col-sm-3 icon-block">        
-                    <div class="well wie">
-                        <h2><span class="icon-title"><img class="logo" src="<?php echo img("logos/Tree_logo.png");?>"><span>Wie</span></span></h2>
-                        <h1>Genealogische Databank Limburg</h1>
-                        <p class="lees-meer">
-                            <a role="button" href="#">Lees Meer</a>
-                        </p>
-                    </div>
+        <h3  class="ontdek"><span>Onze projecten</span></h3>
+        <div class="row icons">
+            <div class="col-sm-3 icon-block">
+                <div class="well wie">
+                    <h2><span class="icon-title"><img class="logo" src="<?php echo img("logos/Tree_logo.png");?>"><span>Wie</span></span></h2>
+                    <h1>Genealogische Databank Limburg</h1>
+                    <p class="lees-meer">
+                        <a role="button" href="#">Lees Meer</a>
+                    </p>
                 </div>
-                <div class="col-sm-3 icon-block">     
-                    <div class="well wat">
-                        <h2><span class="icon-title"><img class="logo" src="<?php echo img("logos/book_logo.png");?>"><span>Wat</span></span></h2>
-                        <h1>Genealogische Databank Limburg</h1>
-                        <p class="lees-meer">
-                            <a role="button" href="#">Lees Meer</a>
-                        </p>
-                    </div>
-                </div> 
-
-                <div class="col-sm-3 icon-block">        
-                    <div class="well waar">
-                        <h2><span class="icon-title"><img class="logo" src="<?php echo img("logos/earth_logo.png");?>"><span>Waar</span></span></h2>
-                        <h1>Genealogische Databank Limburg</h1>
-                        <p class="lees-meer">
-                            <a role="button" href="#">Lees Meer</a>
-                        </p>
-                    </div>
-                </div>
-                <div class="col-sm-3 icon-block">     
-                    <div class="well wanneer">
-                        <h2><span class="icon-title"><img class="logo" src="<?php echo img("logos/clock_logo.png");?>"><span>Wanneer</span></span></h2>
-                        <h1>Genealogische Databank Limburg</h1>
-                        <p class="lees-meer">
-                            <a role="button" href="#">Lees Meer</a>
-                        </p>
-                    </div>
-                </div>     
-           
-        </div>
-    </div>   
-   </section>   
-    <div class="container content-wrapper bs-docs-section "> 
-    
-        <div class="row">
-            <div class="col-sm-5">                  
-                <section class="intro"> 
-                <H3><span>Over ons</span></h3>    
-                <h1>bewoners en bewoningsgeschiedenis</h1>
-                <!--<img src="<?php echo img("roermond.jpg");?>">
-                <p class="caption">Markt Roermond 19xx</p>-->
-
-                <p>Het <b>Aezel Projek</b>, ofwel “Archief voor Erfgoed van Zuid-Nederlandse Eigendommen en Leefgemeenschappen”, heeft tot doel de versnipperde erfgoed-informatie in Zuid Nederland te digitaliseren, te bundelen en voor het brede publiek beschikbaar te stellen via het internet.</p>        
-                <p class="lees-meer"><a  href="#" role="button">Lees meer</a></p>     
-                </section>
-            </div>   
-
-            <div class="col-sm-7">
-                <section class='nieuws'>
-                    <h3><span>Nieuws</span></h3>
-                    <div class="row nieuws-item">
-                        <div class="col-sm-3 icon-block"><img src="<?php echo img("bg6.png");?>"></div>  
-                        <div class="col-sm-9 icon-block">
-                            <h5>14-08-2016</h5>
-                            <h4>Fusce dapibus</h4>
-                            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                            <p class="lees-meer"><a href="#" role="button">Lees Meer</a></p>
-                        </div>
-                    </div>
-                    <div class="row nieuws-item">
-                        <div class="col-sm-3 icon-block"><img src="<?php echo img("foto.jpg");?>"></div>  
-                        <div class="col-sm-9 icon-block">
-                            <h5>14-08-2016</h5>
-                            <h4>Etiam porta sem malesuada</h4>
-                            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                            <p class="lees-meer"><a href="#" role="button">Lees Meer</a></p>
-                        </div>    
-                    </div>
-                    <div class="row nieuws-item">
-                        <div class="col-sm-3 icon-block"><img src="<?php echo img("bg5.jpg");?>"></div>  
-                        <div class="col-sm-9 icon-block">
-                            <h5>14-08-2016</h5>
-                            <h4>Cras justo odio</h4>
-                            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                            <p class="lees-meer"><a  href="#" role="button">Lees Meer</a></p>
-                        </div>    
-                    </div>        
-                </section>
             </div>
+            <div class="col-sm-3 icon-block">
+                <div class="well wat">
+                    <h2><span class="icon-title"><img class="logo" src="<?php echo img("logos/book_logo.png");?>"><span>Wat</span></span></h2>
+                    <h1>Genealogische Databank Limburg</h1>
+                    <p class="lees-meer">
+                        <a role="button" href="#">Lees Meer</a>
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-3 icon-block">
+                <div class="well waar">
+                    <h2><span class="icon-title"><img class="logo" src="<?php echo img("logos/earth_logo.png");?>"><span>Waar</span></span></h2>
+                    <h1>Genealogische Databank Limburg</h1>
+                    <p class="lees-meer">
+                        <a role="button" href="#">Lees Meer</a>
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-3 icon-block">
+                <div class="well wanneer">
+                    <h2><span class="icon-title"><img class="logo" src="<?php echo img("logos/clock_logo.png");?>"><span>Wanneer</span></span></h2>
+                    <h1>Genealogische Databank Limburg</h1>
+                    <p class="lees-meer">
+                        <a role="button" href="#">Lees Meer</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="container content-wrapper bs-docs-section ">
+    <div class="row">
+        <div class="col-sm-5">
+            <section class="intro">
+            <H3><span>Over ons</span></h3>
+            <h1>bewoners en bewoningsgeschiedenis</h1>
+            <!--<img src="<?php echo img("roermond.jpg");?>">
+            <p class="caption">Markt Roermond 19xx</p>-->
 
-        </div>  
-    </div>    
+            <p>Het <b>Aezel Projek</b>, ofwel “Archief voor Erfgoed van Zuid-Nederlandse Eigendommen en Leefgemeenschappen”, heeft tot doel de versnipperde erfgoed-informatie in Zuid Nederland te digitaliseren, te bundelen en voor het brede publiek beschikbaar te stellen via het internet.</p>
+            <p class="lees-meer"><a  href="#" role="button">Lees meer</a></p>
+            </section>
+        </div>
 
-
+        <div class="col-sm-7">
+            <section class='nieuws'>
+                <h3><span>Nieuws</span></h3>
+                <div class="row nieuws-item">
+                    <div class="col-sm-4 icon-block"><img src="<?php echo img("bg6.png");?>"></div>
+                    <div class="col-sm-8 icon-block">
+                        <h5>14-08-2016</h5>
+                        <h4>Fusce dapibus</h4>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p class="lees-meer"><a href="#" role="button">Lees Meer</a></p>
+                    </div>
+                </div>
+                <div class="row nieuws-item">
+                    <div class="col-sm-4 icon-block"><img src="<?php echo img("foto.jpg");?>"></div>
+                    <div class="col-sm-8 icon-block">
+                        <h5>14-08-2016</h5>
+                        <h4>Etiam porta sem malesuada</h4>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p class="lees-meer"><a href="#" role="button">Lees Meer</a></p>
+                    </div>
+                </div>
+                <div class="row nieuws-item">
+                    <div class="col-sm-4 icon-block"><img src="<?php echo img("bg5.jpg");?>"></div>
+                    <div class="col-sm-8 icon-block">
+                        <h5>14-08-2016</h5>
+                        <h4>Cras justo odio</h4>
+                        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                        <p class="lees-meer"><a  href="#" role="button">Lees Meer</a></p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</div>
 <script>
     jQuery('.carousel').carousel();
-    var caption = jQuery('div.item:nth-child(1) .carousel-caption');
+    var caption = jQuery('div.carousel-item:nth-child(1) .carousel-caption');
     jQuery('#right .well').html(caption.html());
     caption.css('display','none');
     jQuery(".carousel").on('slide.bs.carousel', function(evt) {
-
-
-       var caption = jQuery('div.item:nth-child(' + (jQuery(evt.relatedTarget).index()+1) + ') .carousel-caption');
+       var caption = jQuery('div.carousel-item:nth-child(' + (jQuery(evt.relatedTarget).index()+1) + ') .carousel-caption');
        jQuery('#right .well').html(caption.html());
        caption.css('display','none');
     });
-</script>    
+</script>
 
 <?php echo foot(); ?>
